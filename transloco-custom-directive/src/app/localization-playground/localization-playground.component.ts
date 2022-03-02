6c642157884cb11e87e1f1716b5ed77929cb3951
Yoni12ab <yoni12ab@gmail.com>
2022-03-02 15:40:12 +0200

    first commit

--- transloco-custom-directive/src/app/localization-playground/localization-playground.component.ts
+++ transloco-custom-directive/src/app/localization-playground/localization-playground.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TranslocoService } from '@ngneat/transloco';
+import { NgForm } from '@angular/forms';
 
 enum TranslationMethods {
   TRANSLOCO_DIRECTIVE = 'TRANSLOCO_DIRECTIVE',
@@ -8,34 +9,60 @@ enum TranslationMethods {
   MGMT_WRAPPER_DIRECTIVE = 'MGMT_WRAPPER_DIRECTIVE',
 }
 
+const LOCAL_STORAGE_LAST_TRANSLATE_OBJ = 'lastTranslateObj';
+
 @Component({
   selector: 'mgmt-localization-playground',
   templateUrl: './localization-playground.component.html',
   styleUrls: ['./localization-playground.component.scss'],
 })
 export class LocalizationPlaygroundComponent implements OnInit {
-  public numberOfRows = 100;
-  public numberOfCells = 100;
+  @ViewChild('translateForm', { static: true }) translateForm:
+    | NgForm
+    | undefined;
   public rows = [];
   public cells = [];
   public TranslationMethods = TranslationMethods;
-  public methodToUse = TranslationMethods.TRANSLOCO_DIRECTIVE;
+  public translateFormValues: {
+    methodToUse: TranslationMethods;
+    numberOfRows: number;
+    numberOfCells: number;
+    lang: string;
+    multiplier: number;
+  };
 
-  constructor(private translocoService: TranslocoService) {}
+  constructor(private translocoService: TranslocoService) {
+    const lastFormValues = localStorage.getItem(
+      LOCAL_STORAGE_LAST_TRANSLATE_OBJ
+    );
+    this.translateFormValues = lastFormValues
+      ? JSON.parse(lastFormValues)
+      : {
+          methodToUse: TranslationMethods.TRANSLOCO_DIRECTIVE,
+          numberOfRows: 200,
+          numberOfCells: 200,
+          lang: 'en',
+          multiplier: 1,
+        };
+  }
 
   ngOnInit(): void {
-    this.initRowsAndColumns();
+    this.translateForm?.valueChanges?.subscribe(this.initForm.bind(this));
+    this.languageChange(this.translateFormValues?.lang);
   }
 
-  public initRowsAndColumns() {
-    this.rows = Array.from({ length: this.numberOfRows });
-    this.cells = Array.from({ length: this.numberOfCells });
+  public initForm() {
+    this.rows = Array.from({ length: this.translateFormValues.numberOfRows });
+    this.cells = Array.from({ length: this.translateFormValues.numberOfRows });
+    localStorage.setItem(
+      LOCAL_STORAGE_LAST_TRANSLATE_OBJ,
+      JSON.stringify(this.translateFormValues)
+    );
   }
 
-  languageChange(event: any) {
-    //this.translocoService.setActiveLang(event.value);
-    this.translocoService.load(event.value).subscribe(() => {
-      this.translocoService.setActiveLang(event.value);
+  public languageChange(lang: string) {
+    this.translocoService.load(lang).subscribe(() => {
+      this.translocoService.setActiveLang(lang);
     });
   }
 }
